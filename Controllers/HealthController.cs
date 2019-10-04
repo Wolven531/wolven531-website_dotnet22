f3c1104b4d7bcdaf683e39b2f1cdd97de44457b7
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-03 23:01:39 -0600

    Add health controller

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace wolven531WebsiteDotnet22.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new JObject { ["healthy"] = true });
+        }
+    }
+}
