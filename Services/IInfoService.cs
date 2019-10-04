28456156c5fa3516c063ab7f40a988e346aa501e
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-03 23:25:01 -0600

    Add, implement IInfoService

---
+++ Services/IInfoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wolven531WebsiteDotnet22.Services
+{
+    public interface IInfoService
+    {
+        string GetFormattedStartTime();
+    }
+}
---
+++ Services/InfoService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wolven531WebsiteDotnet22.Services
+{
+    public class InfoService : IInfoService
+    {
+        private string startupTime;
+
+        public InfoService()
+        {
+            var startTime = DateTime.UtcNow;
+            startupTime = $"{startTime.ToLongDateString()} {startTime.ToLongTimeString()}";
+        }
+
+        public string GetFormattedStartTime()
+        {
+            return startupTime;
+        }
+    }
+}
