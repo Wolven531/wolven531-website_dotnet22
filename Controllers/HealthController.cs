eba5012bf31cffe7941800aeb49026c33169d244
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-03 23:25:38 -0600

    Use IInfoService to add startupTime to health check

--- Controllers/HealthController.cs
+++ Controllers/HealthController.cs
@@ -4,17 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
+using wolven531WebsiteDotnet22.Services;
 
 namespace wolven531WebsiteDotnet22.Controllers
 {
     [Route("api/[controller]")]
     public class HealthController : Controller
     {
+        private readonly IInfoService _infoService;
+
+        public HealthController(IInfoService infoService)
+        {
+            _infoService = infoService;
+        }
+
         // GET: api/<controller>
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(new JObject { ["healthy"] = true });
+            return Ok(new JObject {
+                ["healthy"] = true,
+                ["startupTime"] = _infoService.GetFormattedStartTime()
+            });
         }
     }
 }
