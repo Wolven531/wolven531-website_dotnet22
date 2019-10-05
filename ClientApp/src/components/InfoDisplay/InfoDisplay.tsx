aa796bd26027fc424490e8bd5f3411ee48ad5d8c
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-04 22:26:26 -0600

    Add InfoDisplay component

---
+++ ClientApp/src/components/InfoDisplay/InfoDisplay.tsx
@@ -0,0 +1,30 @@
+﻿import React, { Component } from 'react'
+
+class InfoDisplay extends Component<{}, { data: any, loading: boolean }> {
+	constructor(props) {
+		super(props)
+		this.state = { data: null, loading: true }
+
+		fetch('api/health')
+			.then(response => response.json())
+			.then(data => {
+				this.setState({ data, loading: false })
+			})
+	}
+
+	public render() {
+		return (
+			<article>
+				<h2>Server Info</h2>
+				{this.state.loading
+					? <p>Loading...</p>
+					: <section>
+						<p>{(JSON.stringify(this.state.data, null, 4))}</p>
+					</section>
+				}
+			</article>
+		)
+	}
+}
+
+export { InfoDisplay }
