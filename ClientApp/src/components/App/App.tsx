d5538e8866316f61a5ad4f62f91ee7ee82f92c8b
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-04 22:27:38 -0600

    Add InfoDisplay to App

--- ClientApp/src/components/App/App.tsx
+++ ClientApp/src/components/App/App.tsx
@@ -4,6 +4,7 @@ import { Route } from 'react-router'
 import { Counter } from '../Counter/Counter'
 import { FetchData } from '../FetchData/FetchData'
 import { Home } from '../Home/Home'
+import { InfoDisplay } from '../InfoDisplay/InfoDisplay'
 import { Layout } from '../Layout/Layout'
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
 				<Route exact path='/' component={Home} />
 				<Route path='/counter' component={Counter} />
 				<Route path='/fetch-data' component={FetchData} />
+				<Route path='/info' component={InfoDisplay} />
 			</Layout>
 		)
 	}
