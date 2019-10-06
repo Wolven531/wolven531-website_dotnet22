a9ef15df442c73b982cc2121b8cd820a75b706d5
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-05 22:54:10 -0600

    Fixed UniquePageHit logic for production PATH

--- Startup.cs
+++ Startup.cs
@@ -69,7 +69,8 @@ namespace wolven531WebsiteDotnet22
             app.Use(async (context, next) =>
             {
                 if (context.Request.Method.Equals("get", StringComparison.OrdinalIgnoreCase) &&
-                    context.Request.Path.HasValue && context.Request.Path.Value.Equals("/static/js/main.chunk.js", StringComparison.OrdinalIgnoreCase))
+                    // TODO: would like to use StartsWithSegments here:
+                    context.Request.Path.HasValue && context.Request.Path.Value.StartsWith("/static/js/main", StringComparison.OrdinalIgnoreCase))
                 {
                     _logger.LogDebug("[MIDDLEWARE FOR UNIQUE]\t\t\tMarking UNIQUE visit and calling next...");
                     _infoService.AddUniquePageHit();
