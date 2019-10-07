b7037fc13876ca5acded40dfa07fbcb688b0f6e9
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-06 21:38:48 -0600

    Convert Counter to use hooks

--- ClientApp/src/components/Counter/Counter.tsx
+++ ClientApp/src/components/Counter/Counter.tsx
@@ -1,28 +1,43 @@
-import React, { Component } from 'react'
+import React, { FC, useState } from 'react'
 
-class Counter extends Component<{}, { currentCount: number }> {
-	constructor(props) {
-		super(props)
-		this.state = { currentCount: 0 }
-		this.incrementCounter = this.incrementCounter.bind(this)
-	}
+const Counter: FC<any> = (props) => {
+	const [currentCounter, setCurrentCounter] = useState(0)
+	const incrementCounter = () => setCurrentCounter(staleCounter => staleCounter + 1)
 
-	public render() {
-		return (
-			<div>
-				<h1>Counter</h1>
-				<p>This is a simple example of a React component.</p>
-				<p>Current count: <strong>{this.state.currentCount}</strong></p>
-				<button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-			</div>
-		)
-	}
-
-	private incrementCounter() {
-		this.setState({
-			currentCount: this.state.currentCount + 1
-		})
-	}
+	return (
+		<div>
+			<h1>Counter</h1>
+			<p>This is a simple example of a React component.</p>
+			<p>Current count: <strong>{currentCounter}</strong></p>
+			<button className="btn btn-primary" onClick={incrementCounter}>Increment</button>
+		</div>
+	)
 }
+
+// NOTE: non-hooks version:
+//class Counter extends Component<{}, { currentCount: number }> {
+//	constructor(props) {
+//		super(props)
+//		this.state = { currentCount: 0 }
+//		this.incrementCounter = this.incrementCounter.bind(this)
+//	}
+
+//	public render() {
+//		return (
+//			<div>
+//				<h1>Counter</h1>
+//				<p>This is a simple example of a React component.</p>
+//				<p>Current count: <strong>{this.state.currentCount}</strong></p>
+//				<button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+//			</div>
+//		)
+//	}
+
+//	private incrementCounter() {
+//		this.setState({
+//			currentCount: this.state.currentCount + 1
+//		})
+//	}
+//}
 
 export { Counter }
