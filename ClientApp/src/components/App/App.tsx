93d5a8eadcc1afb97bdc356fccaacad47bd18d61
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-07 23:15:14 -0600

    Add a POST request on mount to update unique page hits

--- ClientApp/src/components/App/App.tsx
+++ ClientApp/src/components/App/App.tsx
@@ -8,6 +8,32 @@ import { InfoDisplay } from '../InfoDisplay/InfoDisplay'
 import { Layout } from '../Layout/Layout'
 
 class App extends Component {
+	public componentDidMount() {
+		/*
+			method: 'POST', // *GET, POST, PUT, DELETE, etc.
+			mode: 'cors', // no-cors, *cors, same-origin
+			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			credentials: 'same-origin', // include, *same-origin, omit
+			headers: {
+			  'Content-Type': 'application/json'
+			  // 'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			redirect: 'follow', // manual, *follow, error
+			referrer: 'no-referrer', // no-referrer, *client
+		 */
+		fetch('/api/Info', {
+			cache: 'no-cache',
+			credentials: 'same-origin',
+			method: 'post'
+		} as RequestInit)
+			.then(() => {
+				console.log(`[ componentDidMount | App ] pinged for unique page hit`)
+			})
+			.catch(err => {
+				console.error(`An error ocurred`, err)
+			})
+	}
+
 	public render() {
 		return (
 			<Layout>
