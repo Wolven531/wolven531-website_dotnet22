05101e0a7d3ff944180f03f727e919b099be2390
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-10 17:46:42 -0600

    Set page title and alter greeting on Home

--- ClientApp/src/components/Home/Home.tsx
+++ ClientApp/src/components/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React, { PureComponent } from 'react'
 
 class Home extends PureComponent {
+	public componentDidMount() {
+		window.document.title = 'Wolven531 Web'
+	}
+
 	public render() {
 		return (
 			<div>
-				<h1>Hello, world!</h1>
+				<h1>Hello, person!</h1>
 			</div>
 		)
 	}
