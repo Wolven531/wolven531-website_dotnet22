c8e36e2ac233009baf76a9cfe1169cc37724b06f
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-09 22:12:37 -0600

    Import constants

---
+++ ClientApp/src/constants.ts
@@ -0,0 +1,13 @@
+export const GATHERER_COST = 10
+export const GATHERER_INCOME = 1
+export const GATHERER_INITIAL_TICK = 0
+export const GATHERER_MAX_SPEED = 10
+export const GATHERER_TICK_RATE = 10
+export const GATHERER_TIME_SECONDS = 2
+
+export const STORAGEKEY_ACHIEVEMENT_LEVEL_GATHER = 'critter-manager.achievements.gather'
+export const STORAGEKEY_ACHIEVEMENT_LEVEL_MONEY = 'critter-manager.achievements.money'
+export const STORAGEKEY_GATHERCOUNT = 'critter-manager.gatherers'
+export const STORAGEKEY_GATHERINCOMELEVEL = 'critter-manager.gatherROILevel'
+export const STORAGEKEY_GATHERSPEEDLEVEL = 'critter-manager.gatherSpeedLevel'
+export const STORAGEKEY_MONEY = 'critter-manager.money'
