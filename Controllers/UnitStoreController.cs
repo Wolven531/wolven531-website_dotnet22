7bc6d50f1e2fa8787bbd32de7b1f5ac36557c933
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-21 22:20:31 -0600

    Expose new service method via UnitStore controller

--- Controllers/UnitStoreController.cs
+++ Controllers/UnitStoreController.cs
@@ -18,18 +18,25 @@ namespace wolven531WebsiteDotnet22.Controllers
             _unitStore = unitStore;
         }
 
+        // GET api/units/info
+        [HttpGet("info")]
+        public JObject GetCompleteUnitInfo()
+        {
+            return _unitStore.GetCompleteUnitInfo();
+        }
+
         // GET api/units/cost/2
         [HttpGet("cost/{id}")]
         public JObject GetUnitCost(int id)
         {
-            return _unitStore.getUnitCost(id);
+            return _unitStore.GetUnitCost(id);
         }
 
         // GET api/units/info/2
         [HttpGet("info/{id}")]
         public JObject GetUnitInfo(int id)
         {
-            return _unitStore.getUnitInfo(id);
+            return _unitStore.GetUnitInfo(id);
         }
     }
 }
