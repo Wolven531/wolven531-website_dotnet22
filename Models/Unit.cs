1b7c742a4b316cd642d395652a80042adeb364e8
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-22 21:45:30 -0600

    Add Unit CS model

---
+++ Models/Unit.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wolven531WebsiteDotnet22.Models
+{
+    public class Unit
+    {
+        public JObject Cost { get; set; }
+
+        public int Id { get; set; }
+
+        public JObject Info { get; set; }
+
+        public string Name { get; set; }
+    }
+}
