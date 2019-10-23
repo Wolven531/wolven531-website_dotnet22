9f2f0e2a068d1d22ffa5c43af82cf494a7f25587
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-22 21:46:11 -0600

    Convert unit info endpoint to return more consumable list

--- Controllers/UnitStoreController.cs
+++ Controllers/UnitStoreController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
+using wolven531WebsiteDotnet22.Models;
 using wolven531WebsiteDotnet22.Services;
 
 namespace wolven531WebsiteDotnet22.Controllers
@@ -20,9 +21,18 @@ namespace wolven531WebsiteDotnet22.Controllers
 
         // GET api/units/info
         [HttpGet("info")]
-        public JObject GetCompleteUnitInfo()
+        public IEnumerable<Unit> GetCompleteUnitInfo()
         {
-            return _unitStore.GetCompleteUnitInfo();
+            var unitMap = _unitStore.GetCompleteUnitInfo();
+            var units = new List<Unit>();
+
+            foreach (var unitJson in unitMap)
+            {
+                var unit = unitJson.Value.ToObject<Unit>();
+                units.Add(unit);
+            }
+
+            return units;
         }
 
         // GET api/units/cost/2
