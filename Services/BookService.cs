9c3f5fe469a40ccc59bb5f114b27f6deb5ef8bab
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-23 23:32:19 -0600

    Add BookService

---
+++ Services/BookService.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wolven531WebsiteDotnet22.Models;
+
+namespace wolven531WebsiteDotnet22.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> GetAllBooks() =>
+            _books.Find(book => true).ToList();
+
+        public Book GetBook(string id) =>
+            _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn) =>
+            _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(Book bookIn) =>
+            _books.DeleteOne(book => book.Id == bookIn.Id);
+
+        public void Remove(string id) =>
+            _books.DeleteOne(book => book.Id == id);
+    }
+}
---
+++ Services/IBookService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wolven531WebsiteDotnet22.Models;
+
+namespace wolven531WebsiteDotnet22.Services
+{
+    public interface IBookService
+    {
+        List<Book> GetAllBooks();
+
+        Book GetBook(string id);
+
+        Book Create(Book book);
+
+        void Remove(Book bookIn);
+
+        void Remove(string id);
+
+        void Update(string id, Book bookIn);
+    }
+}
