73c4c4b45820e289d6fbf2c7540b562f86cc30c3
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-27 18:30:37 -0600

    Use new emoji comps in UnitDisplay

--- ClientApp/src/components/UnitDisplay/UnitDisplay.tsx
+++ ClientApp/src/components/UnitDisplay/UnitDisplay.tsx
@@ -6,6 +6,10 @@ import {
 
 import { Unit } from '../../models/Unit'
 
+import { FoodEmoji } from '../Emoji/FoodEmoji'
+import { StoneEmoji } from '../Emoji/StoneEmoji'
+import { WoodEmoji } from '../Emoji/WoodEmoji'
+
 interface IUnitDisplayProps {
 	unit: Unit
 }
@@ -19,9 +23,9 @@ const UnitDisplay: FC<IUnitDisplayProps> = memo(({ unit }) => {
 			<p>{unit.Name}</p>
 			<p>{unit.Info.Description}</p>
 			<ul className="unit-cost">
-				{unit.Cost.Food > 0 && <li>Food (🥩): {unit.Cost.Food}</li>}
-				{unit.Cost.Stone > 0 && <li>Stone (⛰): {unit.Cost.Stone}</li>}
-				{unit.Cost.Wood > 0 && <li>Wood (🌳): {unit.Cost.Wood}</li>}
+				{unit.Cost.Food > 0 && <li>Food (<FoodEmoji />): {unit.Cost.Food}</li>}
+				{unit.Cost.Stone > 0 && <li>Stone (<StoneEmoji />): {unit.Cost.Stone}</li>}
+				{unit.Cost.Wood > 0 && <li>Wood (<WoodEmoji />): {unit.Cost.Wood}</li>}
 			</ul>
 			{/*
 			<br />
