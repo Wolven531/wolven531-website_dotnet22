a4b05d01c3541938a5a498f607dcc73495050f51
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-28 22:05:02 -0600

    Handle addMoney and setMoney in reducer

--- ClientApp/src/redux/reducers/appReducer.ts
+++ ClientApp/src/redux/reducers/appReducer.ts
@@ -1,7 +1,12 @@
-import { SET_APP_LOADED } from '../actionTypes'
+import {
+	ADD_MONEY,
+	SET_APP_LOADED,
+	SET_MONEY
+} from '../actionTypes'
 
 const initialState = {
-	appIsLoaded: false
+	appIsLoaded: false,
+	money: 0
 }
 
 const appReducer = (state = initialState, action) => {
@@ -11,11 +16,19 @@ const appReducer = (state = initialState, action) => {
 				...state,
 				appIsLoaded: action.payload
 			};
-			return state;
-		// case DELETE_ITEM:
-		//   state = {
-		//   };
-		//   return state;
+			return state
+		case ADD_MONEY:
+			state = {
+				...state,
+				money: state.money + action.payload
+			}
+			return state
+		case SET_MONEY:
+			state = {
+				...state,
+				money: action.payload
+			}
+			return state
 		default:
 			return state
 	}
