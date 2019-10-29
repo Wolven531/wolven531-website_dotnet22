fad3983f718fea3e1ef316fc5e368bcec0e6a3d2
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-28 22:07:43 -0600

    Make appReducer available properly

--- ClientApp/src/redux/store/index.ts
+++ ClientApp/src/redux/store/index.ts
@@ -1,24 +1,26 @@
-// import * as WeatherForecasts from './WeatherForecasts'
-// import * as Counter from './Counter'
-import { rootReducer } from '../reducers'
+// import { rootReducer } from '../reducers'
+import { appReducer } from '../reducers/appReducer'
 
 // The top-level state object
 export interface ApplicationState {
-    // counter: Counter.CounterState | undefined
-    // weatherForecasts: WeatherForecasts.WeatherForecastsState | undefined
+	// counter: Counter.CounterState | undefined
+	// weatherForecasts: WeatherForecasts.WeatherForecastsState | undefined
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
-// the reducer with the matching name. It's important that the names match exactly, and that the reducer
-// acts on the corresponding ApplicationState property type.
+// the reducer with the matching name. Important: names must match exactly, and that the reducer
+// acts on the corresponding ApplicationState property type
 // export const reducers = {
 //     counter: Counter.reducer,
 //     weatherForecasts: WeatherForecasts.reducer
 // };
-export const reducers = rootReducer
+// export const reducers = rootReducer
+export const reducers = {
+	appReducer
+}
 
 // This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
-// correctly typed to match your store.
+// correctly typed to match your store
 export interface AppThunkAction<TAction> {
-    (dispatch: (action: TAction) => void, getState: () => ApplicationState): void
+	(dispatch: (action: TAction) => void, getState: () => ApplicationState): void
 }
