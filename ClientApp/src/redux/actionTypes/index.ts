09a9d4b5a1111106cb98e573322eb0537b1319d7
Anthony Williams <anthony.williams.cs@gmail.com>
2019-10-29 21:46:51 -0600

    Add action types for food, stone, and wood

--- ClientApp/src/redux/actionTypes/index.ts
+++ ClientApp/src/redux/actionTypes/index.ts
@@ -1,6 +1,12 @@
 // NOTE: app section
 export const SET_APP_LOADED = 'SET_APP_LOADED'
 
-// NOTE: money section
+// NOTE: resource section
+export const ADD_FOOD = 'ADD_FOOD'
 export const ADD_MONEY = 'ADD_MONEY'
+export const ADD_STONE = 'ADD_STONE'
+export const ADD_WOOD = 'ADD_WOOD'
+export const SET_FOOD = 'SET_FOOD'
 export const SET_MONEY = 'SET_MONEY'
+export const SET_STONE = 'SET_STONE'
+export const SET_WOOD = 'SET_WOOD'
