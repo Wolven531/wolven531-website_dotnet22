7a43e6d45dfe74cfa0a0c6d0aca3f101c5b46d99
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-03 16:29:46 -0700

    Add setFoodCount, expendUnitCost- resource actions

--- ClientApp/src/redux/actions/resourceActions.ts
+++ ClientApp/src/redux/actions/resourceActions.ts
@@ -1,11 +1,25 @@
 import {
 	ADD_MONEY,
+	PURCHASE_UNIT,
+	SET_FOOD,
 	SET_MONEY
 } from '../actionTypes'
+
+import { Unit } from '../../models/Unit'
 
 const redux_addMoney = (additionalAmount: number) => ({
 	payload: additionalAmount,
 	type: ADD_MONEY
+})
+
+const redux_expendUnitCost = (unit: Unit) => ({
+	payload: unit,
+	type: PURCHASE_UNIT
+})
+
+const redux_setFoodCount = (foodCount: number) => ({
+	payload: foodCount,
+	type: SET_FOOD
 })
 
 const redux_setMoney = (newAmount: number) => ({
@@ -15,11 +29,15 @@ const redux_setMoney = (newAmount: number) => ({
 
 const resourceActions = {
 	redux_addMoney,
+	redux_expendUnitCost,
+	redux_setFoodCount,
 	redux_setMoney
 }
 
 export {
 	resourceActions,
 	redux_addMoney,
+	redux_expendUnitCost,
+	redux_setFoodCount,
 	redux_setMoney
 }
