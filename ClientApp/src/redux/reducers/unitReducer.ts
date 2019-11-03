5d6356e003c22ca6291d2f23bbfb6a8522120bc9
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-02 22:23:03 -0600

    First pass at unit count in unit reducer

--- ClientApp/src/redux/reducers/unitReducer.ts
+++ ClientApp/src/redux/reducers/unitReducer.ts
@@ -4,19 +4,25 @@ import {
 
 import { Unit } from '../../models/Unit'
 
-export interface IResourceReducerProps {
+export interface IUnitReducerProps {
+	unitCount: any
 	units: Unit[]
 }
 
-const initialState: IResourceReducerProps = {
+const initialState: IUnitReducerProps = {
+	unitCount: { },
 	units: []
 }
 
 const unitReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case SET_UNITS:
+			const newUnitCount = { };
+			(action.payload as Unit[]).forEach(unit => {
+				newUnitCount[unit.Id] = 0
+			})
 			state = {
-				...state,
+				unitCount: newUnitCount,
 				units: action.payload
 			}
 			return state
