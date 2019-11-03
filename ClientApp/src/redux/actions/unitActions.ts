e6ae279a8942ca3561f296e39e785c43815a2d77
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-03 16:28:36 -0700

    First attempt at using Cost in unit actions

--- ClientApp/src/redux/actions/unitActions.ts
+++ ClientApp/src/redux/actions/unitActions.ts
@@ -1,14 +1,21 @@
+import { Unit } from '../../models/Unit'
+
 import {
 	PURCHASE_UNIT,
 	SET_UNITS
 } from '../actionTypes'
 
-import { Unit } from '../../models/Unit'
+import { redux_expendUnitCost } from './resourceActions'
 
-const redux_purchaseUnit = (unit: Unit) => ({
+const redux_purchase = (unit: Unit) => ({
 	payload: unit,
 	type: PURCHASE_UNIT
 })
+
+const redux_purchaseUnit = (unit: Unit) => dispatch => {
+	dispatch(redux_expendUnitCost(unit))
+	dispatch(redux_purchase(unit))
+}
 
 const redux_setUnits = (units: Unit[]) => ({
 	payload: units,
