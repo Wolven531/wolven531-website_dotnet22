b87fe92aba2bb8283cf1b191e38de5e44ea117ce
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-06 22:17:35 -0700

    Style UnitDisplay name

--- ClientApp/src/components/UnitDisplay/UnitDisplay.tsx
+++ ClientApp/src/components/UnitDisplay/UnitDisplay.tsx
@@ -41,8 +41,10 @@ const UnitDisplay: FC<IUnitDisplayProps> = (props) => {
 
 	return (
 		<article className="unit-display">
-			<p>{unit.Name} (count: {props.unitCount[unit.Id]})</p>
-			<p>{unit.Info.Description}</p>
+			<p className="name">
+				{unit.Name} <span className="count">(count: <span className="value">{props.unitCount[unit.Id]}</span>)</span>
+			</p>
+			<p className="desc">{unit.Info.Description}</p>
 			<ul className="unit-cost">
 				{unit.Cost.Food > 0 && <li>Food (<FoodEmoji />): {unit.Cost.Food}</li>}
 				{unit.Cost.Stone > 0 && <li>Stone (<StoneEmoji />): {unit.Cost.Stone}</li>}
