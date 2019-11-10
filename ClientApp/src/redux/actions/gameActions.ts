a406d76d3db0bbf8f1eda8f8b3caec29ca6ec536
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-09 23:35:42 -0700

    Add game actions for addFood, addStone

--- ClientApp/src/redux/actions/gameActions.ts
+++ ClientApp/src/redux/actions/gameActions.ts
@@ -2,6 +2,8 @@ import { Unit } from '../../models/Unit'
 
 import {
 	ADD_FOOD,
+	ADD_STONE,
+	ADD_WOOD,
 	PURCHASE_UNIT,
 	RESET_UNIT_COUNT,
 	SET_FOOD,
@@ -11,6 +13,16 @@ import {
 const redux_addFood = (additionalAmount: number) => ({
 	payload: additionalAmount,
 	type: ADD_FOOD
+})
+
+const redux_addStone = (additionalAmount: number) => ({
+	payload: additionalAmount,
+	type: ADD_STONE
+})
+
+const redux_addWood = (additionalAmount: number) => ({
+	payload: additionalAmount,
+	type: ADD_WOOD
 })
 
 const redux_purchaseUnit = (unit: Unit) => ({
@@ -35,6 +47,8 @@ const redux_setUnits = (units: Unit[]) => ({
 
 const gameActions = {
 	redux_addFood,
+	redux_addStone,
+	redux_addWood,
 	redux_purchaseUnit,
 	redux_resetUnitCount,
 	redux_setFoodCount,
@@ -44,6 +58,8 @@ const gameActions = {
 export {
 	gameActions,
 	redux_addFood,
+	redux_addStone,
+	redux_addWood,
 	redux_purchaseUnit,
 	redux_resetUnitCount,
 	redux_setFoodCount,
