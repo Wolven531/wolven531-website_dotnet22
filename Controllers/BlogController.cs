cb5efd6526ecaa3ea9bccf3eaa9871264c4716a9
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-11 20:49:43 -0700

    Add Blog Controller

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace wolven531WebsiteDotnet22.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //// GET: Blog/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+    }
+}
