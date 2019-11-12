c06d2baab72a7e2b3f4e2d568aa496f41486f2dc
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-11 21:28:02 -0700

    Add MVC endpoint to serve blog posts

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -15,10 +16,20 @@ namespace wolven531WebsiteDotnet22.Controllers
             return View();
         }
 
-        //// GET: Blog/Details/5
-        //public ActionResult Details(int id)
-        //{
-        //    return View();
-        //}
+        // GET: Blog/Details/5
+        public ActionResult Details(int id)
+        {
+            var blogEntryPath = Path.Combine("Data", "Blog", $"{id}.html");
+
+            if (System.IO.File.Exists(blogEntryPath))
+            {
+                var entryHtml = System.IO.File.ReadAllText(blogEntryPath);
+                ViewBag.EntryHtml = entryHtml;
+            }
+
+            ViewData["Title"] = $"Blog Entry #{id}";
+
+            return View();
+        }
     }
 }
