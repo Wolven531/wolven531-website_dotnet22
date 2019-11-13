d2e498f014d1ad5552c1d87e271f43ba79794193
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-12 20:41:49 -0700

    Comment out remnants of BookService

--- Services/BookService.cs
+++ Services/BookService.cs
@@ -1,43 +1,43 @@
-﻿using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using wolven531WebsiteDotnet22.Models;
+﻿//using MongoDB.Driver;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using wolven531WebsiteDotnet22.Models;
 
-namespace wolven531WebsiteDotnet22.Services
-{
-    public class BookService : IBookService
-    {
-        private readonly IMongoCollection<Book> _books;
+//namespace wolven531WebsiteDotnet22.Services
+//{
+//    public class BookService : IBookService
+//    {
+//        private readonly IMongoCollection<Book> _books;
 
-        public BookService(IBookstoreDatabaseSettings settings)
-        {
-            var client = new MongoClient(settings.ConnectionString);
-            var database = client.GetDatabase(settings.DatabaseName);
+//        public BookService(IBookstoreDatabaseSettings settings)
+//        {
+//            var client = new MongoClient(settings.ConnectionString);
+//            var database = client.GetDatabase(settings.DatabaseName);
 
-            _books = database.GetCollection<Book>(settings.BooksCollectionName);
-        }
+//            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+//        }
 
-        public List<Book> GetAllBooks() =>
-            _books.Find(book => true).ToList();
+//        public List<Book> GetAllBooks() =>
+//            _books.Find(book => true).ToList();
 
-        public Book GetBook(string id) =>
-            _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+//        public Book GetBook(string id) =>
+//            _books.Find<Book>(book => book.Id == id).FirstOrDefault();
 
-        public Book Create(Book book)
-        {
-            _books.InsertOne(book);
-            return book;
-        }
+//        public Book Create(Book book)
+//        {
+//            _books.InsertOne(book);
+//            return book;
+//        }
 
-        public void Update(string id, Book bookIn) =>
-            _books.ReplaceOne(book => book.Id == id, bookIn);
+//        public void Update(string id, Book bookIn) =>
+//            _books.ReplaceOne(book => book.Id == id, bookIn);
 
-        public void Remove(Book bookIn) =>
-            _books.DeleteOne(book => book.Id == bookIn.Id);
+//        public void Remove(Book bookIn) =>
+//            _books.DeleteOne(book => book.Id == bookIn.Id);
 
-        public void Remove(string id) =>
-            _books.DeleteOne(book => book.Id == id);
-    }
-}
+//        public void Remove(string id) =>
+//            _books.DeleteOne(book => book.Id == id);
+//    }
+//}
--- Services/IBookService.cs
+++ Services/IBookService.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using wolven531WebsiteDotnet22.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using wolven531WebsiteDotnet22.Models;
 
-namespace wolven531WebsiteDotnet22.Services
-{
-    public interface IBookService
-    {
-        List<Book> GetAllBooks();
+//namespace wolven531WebsiteDotnet22.Services
+//{
+//    public interface IBookService
+//    {
+//        List<Book> GetAllBooks();
 
-        Book GetBook(string id);
+//        Book GetBook(string id);
 
-        Book Create(Book book);
+//        Book Create(Book book);
 
-        void Remove(Book bookIn);
+//        void Remove(Book bookIn);
 
-        void Remove(string id);
+//        void Remove(string id);
 
-        void Update(string id, Book bookIn);
-    }
-}
+//        void Update(string id, Book bookIn);
+//    }
+//}
