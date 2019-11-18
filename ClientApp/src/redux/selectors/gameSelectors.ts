e1d35cce3c90ea0f641c2725325edefbd7ae5d0e
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-17 22:50:45 -0700

    Add selector - attackSum

--- ClientApp/src/redux/selectors/gameSelectors.ts
+++ ClientApp/src/redux/selectors/gameSelectors.ts
@@ -1,4 +1,5 @@
 import { IApplicationState } from '../store'
+import { Unit } from '../../models/Unit'
 
 const selectCurrentPopulation = (state: IApplicationState): number => {
 	const unitCount = state.gameReducer.unitCount
@@ -11,6 +12,34 @@ const selectCurrentPopulation = (state: IApplicationState): number => {
 	return totalCount
 }
 
+const selectAttackSum = (state: IApplicationState): number => {
+	const unitCount = state.gameReducer.unitCount
+	let totalAttack = 0
+
+	if (state.gameReducer.units.length < 1) {
+		return totalAttack
+	}
+
+	Object.keys(unitCount).forEach(unitId => {
+		const unit: Unit | undefined = state.gameReducer.units.find(unit => String(unit.Id) === unitId)
+
+		if (!unit) {
+			return
+		}
+
+		const attack = unit.Info['Attack']
+
+		if (isNaN(attack)) {
+			return
+		}
+
+		totalAttack += attack
+	})
+
+	return totalAttack
+}
+
 export {
+	selectAttackSum,
 	selectCurrentPopulation
 }
