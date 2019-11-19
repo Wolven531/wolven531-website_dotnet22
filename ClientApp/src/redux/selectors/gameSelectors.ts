7864e0e95d198cff9d1b27fe5d1286a32c9ae566
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-18 20:20:08 -0700

    Add selector - unitCount

--- ClientApp/src/redux/selectors/gameSelectors.ts
+++ ClientApp/src/redux/selectors/gameSelectors.ts
@@ -2,25 +2,36 @@ import { IApplicationState } from '../store'
 import { Unit } from '../../models/Unit'
 
 const selectCurrentPopulation = (state: IApplicationState): number => {
-	const unitCount = state.gameReducer.unitCount
+	const unitCountMap = state.gameReducer.unitCountMap
 	let totalCount = 0
 
-	Object.keys(unitCount).forEach(unitId => {
-		totalCount += unitCount[unitId]
+	Object.keys(unitCountMap).forEach(unitId => {
+		totalCount += unitCountMap[unitId]
 	})
 
 	return totalCount
 }
 
+const selectUnitCount = (unitId: string) => (state: IApplicationState): number => {
+	const unitCountMap = state.gameReducer.unitCountMap
+	const count = unitCountMap[unitId]
+
+	if (!count) {
+		return 0
+	}
+
+	return count
+}
+
 const selectAttackSum = (state: IApplicationState): number => {
-	const unitCount = state.gameReducer.unitCount
+	const unitCountMap = state.gameReducer.unitCountMap
 	let totalAttack = 0
 
 	if (state.gameReducer.units.length < 1) {
 		return totalAttack
 	}
 
-	Object.keys(unitCount).forEach(unitId => {
+	Object.keys(unitCountMap).forEach(unitId => {
 		const unit: Unit | undefined = state.gameReducer.units.find(unit => String(unit.Id) === unitId)
 
 		if (!unit) {
@@ -41,5 +52,6 @@ const selectAttackSum = (state: IApplicationState): number => {
 
 export {
 	selectAttackSum,
-	selectCurrentPopulation
+	selectCurrentPopulation,
+	selectUnitCount
 }
