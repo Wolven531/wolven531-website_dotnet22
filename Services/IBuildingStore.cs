b00a19decc32a6ad241885eb364190edbcc3ff2b
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-21 18:25:35 -0700

    Add IBuildingStore

---
+++ Services/IBuildingStore.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wolven531WebsiteDotnet22.Services
+{
+    public interface IBuildingStore
+    {
+        JObject GetCompleteBuildingInfo();
+
+        JObject GetBuildingCost(int buildingId);
+
+        int GetBuildingCount(int buildingId);
+
+        JObject GetBuildingInfo(int buildingId);
+    }
+}
