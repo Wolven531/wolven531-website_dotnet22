2cfa7eb8b37e5e9e90b2baebc3e50c51e9046124
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-21 18:38:50 -0700

    Add Building CS model

---
+++ Models/Building.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wolven531WebsiteDotnet22.Models
+{
+    public class Building
+    {
+        //[JsonProperty(PropertyName = "Cost")]
+        public JObject Cost { get; set; }
+
+        //[JsonProperty(PropertyName = "Id")]
+        public int Id { get; set; }
+
+        //[JsonProperty(PropertyName = "Info")]
+        public JObject Info { get; set; }
+
+        //[JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+    }
+}
