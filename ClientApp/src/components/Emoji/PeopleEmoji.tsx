d15116b57e72da8df8940f05cab1c8ef692e038a
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-22 22:19:58 -0700

    Add PeopleEmoji

---
+++ ClientApp/src/components/Emoji/PeopleEmoji.tsx
@@ -0,0 +1,9 @@
+import React, { FC, memo } from 'react'
+
+const PeopleEmoji: FC = memo(() => {
+	return (
+		<span role="img" aria-label="People" title="People">👪</span>
+	)
+})
+
+export { PeopleEmoji }
